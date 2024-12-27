176433b2e8c993ea7b21c7fc4391ca92b471ae47
Fabio Flauzino <fabio@mfrural.com.br>
2024-12-27 12:25:26 -0300

    Adicionando itens ao carrinho

--- src/components/loja/Carrinho.tsx
+++ src/components/loja/Carrinho.tsx
@@ -2,16 +2,23 @@ import { useContext } from "react";
 import Area from "../template/Area";
 import CarrinhoVazio from "./CarrinhoVazio";
 import CarrinhoContext from "@/data/context/CarrinhoContext";
+import ItemCarrinhoCard from "./ItemCarrinhoCard";
 
 export default function Carrinho() {
   const { itens } = useContext(CarrinhoContext)
 
   return (
     <Area titulo="Carrinho" cor="green">
-      {itens.length === 0 ? (
-        <CarrinhoVazio />
+      <div className="flex flex-wrap justify-center gap-4">
+        {itens.length === 0 ? (
+          <CarrinhoVazio />
 
-      ) : <div>{itens.length}</div>}
+        ) : (
+          itens.map(item => (
+            <ItemCarrinhoCard key={item.produto.id} item={item} />
+          ))
+        )}
+      </div>
     </Area>
   )
 }
--- src/data/context/CarrinhoContext.tsx
+++ src/data/context/CarrinhoContext.tsx
@@ -15,7 +15,15 @@ export function CarrinhoProvider(props: any) {
   const [itens, setItens] = useState<ItemCarrinho[]>([])
 
   function adicionarItem(produto: Produto) {
-    setItens([...itens, { produto, quantidade: 1 }])
+    const item = itens.find(item => item.produto.id === produto.id) ?? { produto, quantidade: 0 }
+    const novoItem = { ...item, quantidade: item.quantidade + 1 }
+    const outrosItens = itens.filter(item => item.produto.id !== produto.id)
+
+    setItens([...outrosItens, novoItem].sort(ordernarItem))
+  }
+
+  function ordernarItem(a: ItemCarrinho, b: ItemCarrinho) {
+    return a.produto.id - b.produto.id
   }
 
   return (
